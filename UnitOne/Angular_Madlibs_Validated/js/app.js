1803b5b3e7d6ec6226f2db21f4970d6648d4ebcc
mihirkelkar <kelkarmhr@gmail.com>
2015-07-09 16:35:01 -0500

    Adding Angular  App with a validated form input and a submit attached to the form section

---
+++ UnitOne/Angular_Madlibs_Validated/js/app.js
@@ -0,0 +1,38 @@
+angular.module('MyApp', [])
+	.controller('MadCtrl', function($scope){
+		$scope.gender = ""
+		$scope.showresult = false;
+		$scope.submitted = false;
+		$scope.change = function(){
+			if ($scope.gender == "") {
+				$scope.gender = "her";
+				$scope.pro = "s";
+			} else {
+				$scope.gender = "";
+				$scope.pro = "";
+			}
+		};
+
+		$scope.submit = function(){
+			$scope.submitted = true;
+			if($scope.myForm.$valid && $scope.myFormTwo.$valid && $scope.myFormThree.$valid){
+				$scope.showresult = true;
+			}
+		};
+
+		$scope.reset = function(){
+			$scope.showresult = false;
+			$scope.submitted = false;
+			$scope.gender = "";
+			$scope.male = '';
+			$scope.job = '';
+			$scope.celeb = '';
+			$scope.obx = '';
+			$scope.dirty = '';
+			$scope.tedious = '';
+			$scope.skill = '';
+			$scope.adj = '';
+			$scope.num = '';
+
+		};
+});
